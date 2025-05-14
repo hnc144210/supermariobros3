a04d069a403e02b54bda121a28380594f01632bd
hnc144210 <hnchuyen191919@gmail.com>
2025-05-14 22:55:25 +0700

    Sửa lỗi render platform

--- 05-SceneManager/Platform.h
+++ 05-SceneManager/Platform.h
@@ -12,11 +12,12 @@ protected:
 	float cellWidth;
 	float cellHeight;
 	int spriteIdBegin, spriteIdMiddle, spriteIdEnd;
+	int isBlocking; 
 
 public:
 	CPlatform(float x, float y,
 		float cell_width, float cell_height, int length,
-		int sprite_id_begin, int sprite_id_middle, int sprite_id_end) :CGameObject(x, y)
+		int sprite_id_begin, int sprite_id_middle, int sprite_id_end, int isBlocking) :CGameObject(x, y)
 	{
 		this->length = length;
 		this->cellWidth = cell_width;
@@ -24,6 +25,7 @@ public:
 		this->spriteIdBegin = sprite_id_begin;
 		this->spriteIdMiddle = sprite_id_middle;
 		this->spriteIdEnd = sprite_id_end;
+		this->isBlocking = isBlocking;
 	}
 
 	void Render();
